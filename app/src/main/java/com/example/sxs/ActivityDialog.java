2e2420b5b46e0496f5897b36f2d55d56f158018e
jlxhtjx <jlxhtjx@live.com>
2020-03-26 11:18:51 +0800

    ActivityDialog闪退的问题解决了，因为AppCompatActivity没有android:theme="@android:style/Theme.Dialog"

---
+++ app/src/main/java/com/example/sxs/ActivityDialog.java
@@ -0,0 +1,15 @@
+package com.example.sxs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityDialog extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+    }
+}
