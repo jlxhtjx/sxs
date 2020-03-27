8148eaf0272fbc97f608c74e536bd8b9aa5f31b3
jlxhtjx <jlxhtjx@live.com>
2020-03-27 18:20:09 +0800

    March27 最后一次提交

--- app/src/main/java/com/example/sxs/BelowActivity.java
+++ app/src/main/java/com/example/sxs/BelowActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BelowActivity extends Activity {
+    /*这个Activity是用来继承Activity的，因为AppcompatActivity有的theme不兼容，所以
+    Activity_Other->BaseActivity(AppCompatActivity)->BelowActivity(Activity)*/
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
